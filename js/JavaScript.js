e7c13c130b3f70a254c8662a52ffb561711e9306
MBHuman <zenka712@mail.ru>
2019-12-29 18:39:13 +0500

    Вывел надпись о победе

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let consts = {
-    CLOUD_WITH: 500,
-    CLOUD_HEIGHT: 200,
+    CLOUD_WITH: 520,
+    CLOUD_HEIGHT: 220,
     DIAGRAM_HEIGTH: 30,
     DIAGRAM_WIDTH: 380,
     NAME_LOCATION_WIDTH: 115,
@@ -28,27 +28,21 @@ let maximum = function(data) {
 let graph = function(ctx, players, times) {
     let max = maximum(times);
     ctx.fillStyle = '#000';
+    ctx.font = 'Bold';
+    ctx.fillText('Поздравляем вы победили ужастного Гезбо', consts.NAME_LOCATION_WIDTH, 60);
+    ctx.font = 'Sans';
     for(let i = players.length; i >= 1; i--) {
-        if(i == players.length) {
-            ctx.fillText(players[i -1], consts.NAME_LOCATION_WIDTH,282 - 48 * i);
-            ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
-            ctx.fillRect(200 + 5, consts.HIGHEST_NUMBER - 46 * i + 5, (consts.DIAGRAM_WIDTH * times[i - 1] / max), consts.DIAGRAM_HEIGTH);
-            ctx.fillStyle = '#000';
-            ctx.fillRect(200, consts.HIGHEST_NUMBER - 46 * i, (consts.DIAGRAM_WIDTH * times[i - 1] / max), consts.DIAGRAM_HEIGTH);
-        }
-        else {
-            ctx.fillText(players[i - 1], consts.NAME_LOCATION_WIDTH,282 - 48 * i);
-            ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
-            ctx.fillRect(200 + 5, consts.HIGHEST_NUMBER - 46 * i + 5, (consts.DIAGRAM_WIDTH * times[i - 1] / max), consts.DIAGRAM_HEIGTH);
-            ctx.fillStyle = '#000';
-            ctx.fillRect(200, consts.HIGHEST_NUMBER - 46 * i, (consts.DIAGRAM_WIDTH * times[i - 1] / max), consts.DIAGRAM_HEIGTH);
-        }
+        ctx.fillText(players[i -1], consts.NAME_LOCATION_WIDTH,282 - 48 * i);
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+        ctx.fillRect(200 + 5, consts.HIGHEST_NUMBER - 46 * i + 5, (consts.DIAGRAM_WIDTH * times[i - 1] / max), consts.DIAGRAM_HEIGTH);
+        ctx.fillStyle = '#000';
+        ctx.fillRect(200, consts.HIGHEST_NUMBER - 46 * i, (consts.DIAGRAM_WIDTH * times[i - 1] / max), consts.DIAGRAM_HEIGTH);
     }
 }
 
 window.renderStatistics = function(ctx, players, times) {
-    renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
-    renderCloud(ctx, 100, 50, '#fff');    
+    renderCloud(ctx, 110, 40, 'rgba(0, 0, 0, 0.3)');
+    renderCloud(ctx, 100, 30, '#fff');    
 
     graph(ctx, players, times);
 
