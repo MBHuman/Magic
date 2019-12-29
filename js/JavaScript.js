a4eeafb70457efee4725f526b95fad43b8311971
MBHuman <zenka712@mail.ru>
2019-12-29 17:41:33 +0500

    Сделал возможность отслеживания результата каждого игрока и вывод диаграммы, перевернул результаты

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -13,23 +13,45 @@ let renderCloud = function(ctx, x, y, color) {
     ctx.fillRect(x, y, consts.CLOUD_WITH, consts.CLOUD_HEIGHT);
 }
 
-let graph = function(ctx, data) {
-    let names = ['Вы','Коля', 'Петя', 'Валя'];
-    ctx.fillStyle = '#000';
-    for(let i = 1; i <= 4; i++) {
-        ctx.fillText(names[i - 1], consts.NAME_LOCATION_WIDTH,67 + 38 * i);
-        ctx.fillRect(200, 50 + 36 * i, consts.DIAGRAM_WIDTH, consts.DIAGRAM_HEIGTH);
+let maximum = function(data) {
+    let max = 0;
+    for(let i = 0; i < data.length; i++)
+    {
+        if(data[i] > max)
+            max = data[i];
     }
+
+    return max;
 }
 
-
+let graph = function(ctx, data) {
+    let names = ['Коля', 'Петя', 'Валя', 'Вы'];
+    let results = [1124, 1534, 500, 1000];
+    // results.push(data);
+    let max = maximum(results);
+    ctx.fillStyle = '#000';
+    for(let i = names.length; i >= 1; i--) {
+        if(i == 1) {
+            ctx.fillText(names[i -1], consts.NAME_LOCATION_WIDTH,282 - 48 * i);
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+            ctx.fillRect(200 + 5, 255 - 46 * i + 5, (consts.DIAGRAM_WIDTH * results[i - 1] / max), consts.DIAGRAM_HEIGTH);
+            ctx.fillStyle = '#000';
+            ctx.fillRect(200, 255 - 46 * i, (consts.DIAGRAM_WIDTH * results[i - 1] / max), consts.DIAGRAM_HEIGTH);
+        }
+        else {
+            ctx.fillText(names[i - 1], consts.NAME_LOCATION_WIDTH,282 - 48 * i);
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+            ctx.fillRect(200 + 5, 255 - 46 * i + 5, (consts.DIAGRAM_WIDTH * results[i - 1] / max), consts.DIAGRAM_HEIGTH);
+            ctx.fillStyle = '#000';
+            ctx.fillRect(200, 255 - 46 * i, (consts.DIAGRAM_WIDTH * results[i - 1] / max), consts.DIAGRAM_HEIGTH);
+        }
+    }
+}
 
 window.renderStatistics = function(ctx) {
     renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
     renderCloud(ctx, 100, 50, '#fff');    
 
     graph(ctx);
-    // ctx.fillStyle = '#000';
-    // ctx.fillText('Вы', 115, 75);
-    // ctx.fillRect(150, 56, DIAGRAM_WIDTH, DIAGRAM_HEIGTH);
+
 };
