07daf4517fa169ad77cd7aff715b06851bee92d2
MBHuman <zenka712@mail.ru>
2019-12-30 17:19:07 +0500

    Добавил много персонажей и написал логику для их появления в экране похожих

--- js/JavaScript2.js
+++ js/JavaScript2.js
@@ -1,48 +1,48 @@
-'use strict';
+// 'use strict';
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+// var userDialog = document.querySelector('.setup');
+// userDialog.classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
+// document.querySelector('.setup-similar').classList.remove('hidden');
 
-let WIZARD_NAMES = ['Коля Шептун', 'Аркадий', 'Волдевов','МальчикПетя'];
+// let WIZARD_NAMES = ['Коля Шептун', 'Аркадий', 'Волдевов','МальчикПетя'];
 
-// mock
-// Список похожих персонажей
-let wizards = [
-    // name, coat color
-    {
-        name: WIZARD_NAMES[0],
-        coatColor: 'rgb(241, 43, 107)',
-        eyeColor: 'red'
-    },
-    {
-        name: WIZARD_NAMES[1],
-        coatColor: 'rgb(215, 210, 55)',
-        eyeColor: 'black'
-    },
-    {
-        name: WIZARD_NAMES[2],
-        coatColor: 'rgb(101, 67, 109)',
-        eyeColor: 'green'
-    },
-    {
-        name: WIZARD_NAMES[3],
-        coatColor: 'rgb(79, 167, 85)',
-        eyeColor: 'yellow'
-    }
-];
+// // mock
+// // Список похожих персонажей
+// let wizards = [
+//     // name, coat color
+//     {
+//         name: WIZARD_NAMES[0],
+//         coatColor: 'rgb(241, 43, 107)',
+//         eyeColor: 'red'
+//     },
+//     {
+//         name: WIZARD_NAMES[1],
+//         coatColor: 'rgb(215, 210, 55)',
+//         eyeColor: 'black'
+//     },
+//     {
+//         name: WIZARD_NAMES[2],
+//         coatColor: 'rgb(101, 67, 109)',
+//         eyeColor: 'green'
+//     },
+//     {
+//         name: WIZARD_NAMES[3],
+//         coatColor: 'rgb(79, 167, 85)',
+//         eyeColor: 'yellow'
+//     }
+// ];
 
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector
-    ('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+// var similarListElement = document.querySelector('.setup-similar-list');
+// var similarWizardTemplate = document.querySelector
+//     ('#similar-wizard-template')
+//     .content
+//     .querySelector('.setup-similar-item');
 
-for(let i = 0; i < 4; i++) {
-    let wizardElement = similarWizardTemplate.cloneNode(true);
-    wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyeColor;
-    similarListElement.appendChild(wizardElement);
-}
+// for(let i = 0; i < 4; i++) {
+//     let wizardElement = similarWizardTemplate.cloneNode(true);
+//     wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+//     wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+//     wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyeColor;
+//     similarListElement.appendChild(wizardElement);
+// }
--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,6 @@
 'use strict';
 document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
 
 let characters = [
     {
@@ -31,18 +32,83 @@ let characters = [
         name: 'Ничтожный Малый',
         coatColor: 'rgb(97, 154, 101)',
         eyesColor: 'red'
+    },
+    {
+        name: 'Арсений Бутылкин',
+        coatColor: 'rgb(245, 7, 45)',
+        eyesColor: 'blue'
+    },
+    {
+        name: 'Антонин Оградкин',
+        coatColor: 'rgb(99, 134, 98)',
+        eyesColor: 'cyan'
+    },
+    {
+        name: 'Вор в законе',
+        coatColor: 'rgb(66, 176, 231)',
+        eyesColor: 'purple'
+    },
+    {
+        name: 'Мафиозник',
+        coatColor: 'rgb(255, 0, 00)',
+        eyesColor: 'red'
+    },
+    {
+        name: 'Школьный вор',
+        coatColor: 'rgb(90, 254, 11)',
+        eyesColor: 'black'
+    },
+    {
+        name: 'Обжорник',
+        coatColor: 'rgb(21, 89, 193)',
+        eyesColor: 'red'
+    },
+    {
+        name: 'Любитель вилок',
+        coatColor: 'rgb(73, 124, 181)',
+        eyesColor: 'yellow'
     }
 ]
+
+let isHave = function(prop, num) {
+    let flag = false;
+    for(let i = 0; i < prop.nums.length; i++) {
+        if(num == prop.nums[i]) {
+            flag = true;
+            break;
+        }
+    }
+    if(!flag)
+        prop.nums.push(num);
+    return flag;
+}
+
 let chr = characters.slice();
 let rnd = function(chr) {
-    let data = Math.random() * (chr.length);
-    delete chr[data];
-
-    return(chr[data]);
+    let prop = {
+        arr: [],
+        nums: []
+    }
+    while(prop.arr.length != 4) {
+        let random = Math.floor(Math.random() * chr.length);
+        if(!isHave(prop, random)) {
+            prop.arr.push(chr[random]);
+        }
+    }
+    return(prop.arr);
 }
 
-for(let i = 0; i < chr.length; i++) {
-    console.log(rnd(chr));
-}
+let SimilarListElement = document.querySelector('.setup-similar-list');
+let SimilarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
 
+let arr = rnd(chr);
+for(let i = 0; i < 4; i++) {
+    let wizardElement = SimilarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = arr[i].name;
+    wizardElement.querySelector('.wizard-coat').style.fill = arr[i].coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = arr[i].eyesColor;
+    SimilarListElement.appendChild(wizardElement);
+}
 
